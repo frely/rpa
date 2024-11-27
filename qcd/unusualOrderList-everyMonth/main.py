16c47317df465a9a86a89df08ceebd59360e25cf
frely <frely161@gmail.com>
2024-11-27 17:17:50 +0800

    use env

--- qcd/unusualOrderList-everyMonth/main.py
+++ qcd/unusualOrderList-everyMonth/main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 import pandas as pd
@@ -13,7 +14,7 @@ s = requests.Session()
 #         'Content-Type': 'application/x-www-form-urlencoded'}
 # s.post(baseurl + '/web/login', data=payload, headers=headers)
 
-cookie = f'_ga=GA1.1.955101588.1728870958; _tt_enable_cookie=1; _ttp=c9YGrg6VRmM9dlZCiIxCb6Ytqbc; _gcl_au=1.1.508947572.1729495821; _fbp=fb.1.1729495821048.1985399179; tk_or=%22%22; tk_lr=%22%22; Hm_lvt_d3263c891ffd349cffa1e0f8a0a19fda=1729841496; __bid_n=192c296837396484db0b5e; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22192c29683b2c6c-0e3e81ee7113508-4c657b58-2073600-192c29683b3f97%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTkyYzI5NjgzYjJjNmMtMGUzZTgxZWU3MTEzNTA4LTRjNjU3YjU4LTIwNzM2MDAtMTkyYzI5NjgzYjNmOTcifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%22192c29683b2c6c-0e3e81ee7113508-4c657b58-2073600-192c29683b3f97%22%7D; _ga_SRYJX3579Y=GS1.1.1730164049.11.0.1730164051.58.0.857835633; frontend_lang=zh_CN; tz=Asia/Shanghai; session_id=b5cfc2d7482ce247abc46734758ce0027960389a; cids=12'
+cookie = os.getenv('odoo-cookie')
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
         'Content-Type': 'application/json',
         'Cookie': cookie}
